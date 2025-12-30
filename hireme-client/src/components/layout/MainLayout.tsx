b338d543b8f5d325040720ea531cc1dbd14b0a4a
Nguyen Luong Trung <nguyenluongtrung.dev@gmail.com>
2025-12-30 21:38:47 +0700

    System: Create 404 page

---
+++ hireme-client/src/components/layout/MainLayout.tsx
@@ -0,0 +1,36 @@
+"use client";
+
+import { useContext } from "react";
+
+import Footer from "@/components/layout/Footer";
+import Header from "@/components/layout/Header";
+import Sidebar from "@/components/layout/Sidebar";
+
+import { GlobalStateContext } from "@/providers/GlobalStateProvider";
+
+export default function MainLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  const { openHamburgerMenu } = useContext(GlobalStateContext);
+
+  return (
+    <div className="flex min-h-screen w-full flex-col relative">
+      <Header />
+      {openHamburgerMenu && (
+        <div className="h-[calc(100vh_-_180px)] flex-grow bg-white overflow-y-auto no-scrollbar md:hidden">
+          <Sidebar className="!no-scrollbar" />
+        </div>
+      )}
+      <main
+        className={`p-4 flex-grow flex flex-col gap-10 bg-white overflow-y-auto no-scrollbar lg:custom-scrollbar ${
+          openHamburgerMenu ? "hidden md:block" : ""
+        }`}
+      >
+        {children}
+      </main>
+      <Footer />
+    </div>
+  );
+}
